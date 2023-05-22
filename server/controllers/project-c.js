147ebeaca78641575f275922e211d306460f67c2
timenko <timenkosss@mail.ru>
2023-05-22 18:45:21 +1000

    backend reg/log finish

--- server/controllers/project-c.js
+++ server/controllers/project-c.js
@@ -3,8 +3,12 @@ const Post = require('../models/Project');
 class ProjectC {
     async create(req, res) {
         try {
-            const { nameProject, description } = req.body;
-            const project = new Post({ nameProject, description, createdBy: req.user });
+            const { nameProject, description,requirements } = req.body;
+            const Project = await Post.findOne({nameProject})
+            if (Project) {
+                res.json({message: "такой проект уже существует"})
+            }
+            const project = new Post({ nameProject, description,requirements:requirements, createdBy: req.user });
             await project.save();
             res.json({ message: "проект создан", project });
         } catch (error) {
